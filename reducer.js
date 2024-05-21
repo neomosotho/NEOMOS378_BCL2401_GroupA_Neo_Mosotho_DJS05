da0ddf590498ee085656ac8e8d58c7cac12f8a72
Neo Mosotho <156676402+neomosotho@users.noreply.github.com>
2024-05-21 14:13:34 +0200

    Addition of the reducer.js function

--- reducer.js
+++ reducer.js
@@ -1,1 +1,14 @@
 import { initialState } from "./initialState";
+import { action } from "./action";
+
+export function reducer (state = initialState, action) {
+    switch (action, type) {
+        case 'INCREMENT':
+            return state + 1
+        case 'DECREMENT':
+            return state - 1
+        default:
+            return state;    
+    }
+}
+
