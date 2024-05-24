37db600a05ad7d2246c6f432bb8a88e336c3bd5a
Neo Mosotho <156676402+neomosotho@users.noreply.github.com>
2024-05-24 13:52:08 +0200

    Consoling Scenario 4

--- script.js
+++ script.js
@@ -1,4 +1,27 @@
+//Importing 'reducer' and 'action' files
 import { action } from "./action";
 import { reducer } from "./reducer";
 import {createStore} from "./store.js";
 
+// creating the store
+const store = createStore(reducer);
+
+//console.loging Scenario 1
+console.log('Scenario 1: Initial State Verification');
+console.log(store.getState()); 
+
+//console.loging Scenario 2
+console.log('Scenario 2: Incrementing the counter');
+store.dispatch({ type: action.Increment });
+// store.dispatch({ type: action.DECREMENT});
+console.log(store.getState());
+
+//console.loging Scenario 3
+console.log('scenario 3 Decrementing the Counter');
+store.dispatch({ type: action.Decrement});
+console.log(store.getState());
+
+// console.logging Scenario 4
+console.log('Scenario 4: Resetting the counter');
+store.dispatch({type: action.Reset});
+console.log(store.getState());
