40f9120ce5a53cdcd408264da780aa0256ebd31c
Neo Mosotho <156676402+neomosotho@users.noreply.github.com>
2024-05-24 13:59:58 +0200

    Exporting reducer function

--- reducer.js
+++ reducer.js
@@ -3,12 +3,14 @@ import { action } from "./action";
 
 export function reducer (state = initialState, action) {
     switch (action, type) {
-        case 'INCREMENT':
-            return state + 1
-        case 'DECREMENT':
-            return state - 1
-        default:
-            return state;    
+        case action.INCREMENT:
+            return {count: state.count + 1};
+        case action.DECREMENT:
+            return {count: state.count - 1};
+        case action.RESET:
+            return {count: 0};
+            default:
+                return state;    
     }
 }
 
