3567689096b3909659f6d2f618e40899a45a1781
Neo Mosotho <156676402+neomosotho@users.noreply.github.com>
2024-05-24 14:15:42 +0200

    Debugging

--- reducer.js
+++ reducer.js
@@ -1,14 +1,15 @@
-
+//Importing 'initalState' and 'action'
 import { initialState } from './initialState.js';
 import { action } from './action.js';
 
+//Exporting reducer function
 export function reducer (state = initialState, actions) {
     switch (actions, type) {
-        case action.INCREMENT:
+        case action.Increment:
             return {count: state.count + 1};
-        case action.DECREMENT:
+        case action.Decrement:
             return {count: state.count - 1};
-        case action.RESET:
+        case action.Reset:
             return {count: 0};
             default:
                 return state;    
